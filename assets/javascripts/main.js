94db177fab1e740aa1f5665910c6e633005ca820
Malwine <malwine@mailbox.org>
2017-03-15 21:45:46 +0100

    Make nav toggle

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,9 +1,18 @@
 'use strict';
 
+//Adds or removes a class to toggle the menu dropdown
 function dropDown() {
-  console.log("clicked");
-}
+  var header = document.getElementById("header-content");
+  var headerClasses = header.className.split(/\s+/);
+  var index = headerClasses.indexOf("mobile-dropdown");
 
+  if (index >= 0) {
+    headerClasses.splice(index, 1);
+  } else {
+    headerClasses.push("mobile-dropdown");
+  }
+  header.className = headerClasses.join(" ");
+}
 
 var menuButton = document.getElementById('menu-button');
 menuButton.addEventListener('click', dropDown);
